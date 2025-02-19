f197369df2c3e84d2f65c9b47937e3154f6aa211
Yuridiamoss <yg.mosquedavaca@ugto.mx>
2025-02-19 11:18:14 -0600

    Temperatura

---
+++ Feb19/Temperatura.c
@@ -0,0 +1,16 @@
+/*
+19 de Febrero 2025
+*/
+
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+     float TC, TK; 
+     
+     printf("Introduce el valor de la temperatura en Celsius a convertir \n");
+     scanf("%f", &TC);  
+     TK= TC +273.15; 
+     printf("La temperatura en Kelvin es : %f \n", TK);
+     
+}
